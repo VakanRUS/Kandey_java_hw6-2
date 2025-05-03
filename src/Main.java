6d084e73d77b2a495bec0ad491e2e8e4b70bec72
Vakan <vakan1@yandex.ru>
2025-05-03 14:13:28 +0300

    Done some more corrections

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,21 @@
 import java.time.LocalDate;
+
 class Homework {
     public static void main(String[] args) { // Метод main
-        task1();
-        task2();
-        task3();
-    }
-
-    public static void task1() {
+        //task 1
         System.out.println("task 1");
         int year = 2005;
-        leapYear(year);
+        checkLeapYear(year);
         System.out.println();
-    }
 
-    public static void task2() {
+        //task 2
         System.out.println("task 2");
         int currentYear = LocalDate.now().getYear();
-        String osName = "Android";
-        osVersion(osNumber(osName), currentYear);
+        int osType = 0;
+        checkOsVersion(osType, currentYear);
         System.out.println();
-    }
 
-    public static void task3() {
+        //task 3
         System.out.println("task 3");
         int deliveryDistance = 95;
         if (calculateDeliveryDays(deliveryDistance) > 0) {
@@ -32,7 +26,7 @@ class Homework {
         System.out.println();
     }
 
-    public static void leapYear(int year) {
+    public static void checkLeapYear(int year) {
         if (year > 1584 && ((year % 100 > 0 && year % 4 == 0) || year % 400 == 0)) {
             System.out.println(year + " год — високосный год");
         } else if (year < 1584) {
@@ -42,7 +36,7 @@ class Homework {
         }
     }
 
-    public static void osVersion(int os, int year) {
+    public static void checkOsVersion(int os, int year) {
         if (os == 1 && year < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else if (os == 1 && year >= 2015) {
@@ -54,23 +48,13 @@ class Homework {
         } else System.out.println("Неизвестный тип операционной системы");
     }
 
-    public static int osNumber(String name) {
-        if (name.equals("iOS")) {
-            return 0;
-        } else if (name.equals("Android")) {
-            return 1;
-        } else {
-            return 2;
-        }
-    }
-
     public static int calculateDeliveryDays(int distance) {
         if (distance < 20) {
-            return  1;
+            return 1;
         } else if (distance > 20 && distance < 60) {
-            return  2;
+            return 2;
         } else if (distance > 60 && distance < 100) {
-            return  3;
+            return 3;
         } else {
             return 0;
         }
